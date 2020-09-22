37718625a686c39a249b6e822ba93fb974cd7df4
Alessandro Annini <alessandro.annini@nautes.com>
2020-09-22 23:58:59 +0200

    update coda api

--- useAxios.js
+++ useAxios.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
 export const axiosInstance = axios.create({
-  baseURL: 'https://coda.io/apis/v1beta1',
+  baseURL: 'https://coda.io/apis/v1',
   headers: { Authorization: `Bearer ${process.env.CODA_API_KEY}` },
 });
